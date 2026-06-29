df855b2bd8a4ac70eb1b87aef41a80516192f3c5
Kuber Kanav <17156290+kuberkanav@users.noreply.github.com>
2026-06-29 09:07:06 +0530

    RPS

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A simple console only (no GUI) project to demonstrate basic JS program building.
 
 Rock beats Scissors (It can break the Scissors easily), Scissors beat Paper (It can cut the Paper easily) and Paper beats Rock (It can cover the rock easily)
+
+Main branch is my own version with better ulility and edge case considertaions. Branched version caters to Odin assignment.
--- script.js
+++ script.js
@@ -36,10 +36,6 @@ Planning and Pseudocode -
     #store getHumanChoice value in a variable (loop)
         #if loop is false then stop while-loop and display final result.
         #else call the playRound function with loop and the return value of getComputerChoice as arguments. + display the current score.
-
-
-
-
 */
 
 
@@ -130,3 +126,25 @@ while (true){
         alert(`Score is You - ${humanScore} and Me - ${computerScore}`);
     }
 }
+
+/*
+An integer number greater than 1 is called a prime if it cannot be divided without a remainder by anything except 1 and itself.
+
+In other words, n > 1 is a prime if it can’t be evenly divided by anything except 1 and n.
+
+For example, 5 is a prime, because it cannot be divided without a remainder by 2, 3 and 4.
+
+Write the code which outputs prime numbers in the interval from 2 to n.
+
+For n = 10 the result will be 2,3,5,7.
+
+P.S. The code should work for any n, not be hard-tuned for any fixed value. */
+
+/* 
+if p is null, empty or isNaN then we reprompt
+store +prompt in n.
+for loop where i=2;i<n;i++
+    if(n%i===0) or n<2 then output n is not a prime number
+    break
+alert n is a prime number
+*/
